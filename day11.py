143626dcfed7779baae76d7ae5a92c5c44425e52
nibedita21-ai <nibeditaswain351@gmail.com>
2026-01-06 08:16:01 +0530

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,6 @@
+# write a program to input side of a square & print its area.(lets take the value as float value)
+s1 = float(input("side of a square : "))
+area = s1*s1
+print("Area of the Square is : ", area)
+
+
