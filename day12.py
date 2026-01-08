5ff3357754a1a8e74f61b7c2426ded6d7b3bb2cc
nibedita21-ai <nibeditaswain351@gmail.com>
2026-01-08 18:44:54 +0530

    Add files via upload

---
+++ day12.py
@@ -0,0 +1,6 @@
+# write a program to input 2 floating point numbers & print their average.
+
+a = float(input("Enter First Number : "))
+b = float(input("Enter Second Number : "))
+average = (a + b)/2
+print("Average = ", average)
