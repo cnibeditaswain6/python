1190c87a395bff958d692967b82cd32ee1393929
nibedita21-ai <nibeditaswain351@gmail.com>
2026-01-10 19:55:56 +0530

    Add files via upload

---
+++ day13.py
@@ -0,0 +1,6 @@
+# Write a program to input 2 integer numbers, a & b. Print True if a is greater than or equal to b.If not print false.
+
+a = int(input("Enter First Number : "))
+b = int(input("Enter Second Number : "))
+
+print(a >= b)
