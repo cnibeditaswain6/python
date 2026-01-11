30aea4190fbd164b6c35082075abca983699caf3
nibedita21-ai <nibeditaswain351@gmail.com>
2026-01-11 19:23:32 +0530

    Add files via upload

---
+++ day14.py
@@ -0,0 +1,11 @@
+# write a program to perform basic arithmetic operations on two numbers.
+
+a = int(input("Enter First Number : "))
+b = int(input("Enter Second Number : "))
+
+print("Sum = ", a + b)
+print("Product = ", a * b)
+print("Difference = ", a - b)
+print("Quotient = ", a / b)
+print("Remainder = ", a % b)
+print("Square = ", a ** b)
