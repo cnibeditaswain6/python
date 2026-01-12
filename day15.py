a70a759e9e7a8ff7da50bad692360edce2c36e28
nibedita21-ai <nibeditaswain351@gmail.com>
2026-01-12 11:13:46 +0530

    Add files via upload

---
+++ day15.py
@@ -0,0 +1,14 @@
+# strings & conditional statements
+
+# Basic Operations 
+# 1. Concatenation
+str1 = "C Nibedita "
+str2 = "Swain"
+print(str1 + str2)
+
+# 2. Length of string
+str = "Nibedita"
+print(len(str))
+
+a = "School"
+print(len(a))
