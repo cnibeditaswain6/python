a6c70f01b8e8fd7371d5bfb0660d55ca0460b0c4
nibedita21-ai <nibeditaswain351@gmail.com>
2026-01-15 19:56:34 +0530

    Add files via upload

---
+++ day17.py
@@ -0,0 +1,17 @@
+# slicing
+
+str = "KIIT UNIVERSITY"
+print(str[1:7])
+
+
+n = "C Nibedita Swain"
+print(n[3:8])
+
+str = "Nibedita Swain"
+print(str[5:len(str)])
+
+str = "Kiit University"
+print(str[5:len(str)])
+
+a = "Kiit University"
+print(a[0:8])
