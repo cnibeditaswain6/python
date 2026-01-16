c569d4acc6e84c981f389514eb3580cdf7196fa1
nibedita21-ai <nibeditaswain351@gmail.com>
2026-01-16 11:42:08 +0530

    Add files via upload

---
+++ day18.py
@@ -0,0 +1,9 @@
+# positive index & negative index
+
+# Negative index
+
+str = "Apple"
+print(str[-3:-1])  #ending index is not included("e" is not included because it is the ending index)
+
+s = "BrainDead"
+print(s[-8:-3])
