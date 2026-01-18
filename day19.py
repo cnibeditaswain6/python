eff6b5834c0f5e1b237bf2fa0bd4aecc91387cb9
nibedita21-ai <nibeditaswain351@gmail.com>
2026-01-18 11:22:08 +0530

    Add files via upload

---
+++ day19.py
@@ -0,0 +1,10 @@
+# Write a program to input users first name & print its length.
+
+# user = str(input("Users First Name : "))
+# print("Length of your Name is : ", len(user))
+
+
+# Write a program to find the occurrence of "$" in a string.
+
+str = "Hlw Mys$elf Nibe$dita$ $Swain"
+print("The Occurrence of $ is : ", str.count("$"))
