4a85482905febbcdd1fc3ea3e7a4f084d9185747
nibedita21-ai <nibeditaswain351@gmail.com>
2026-01-20 19:13:57 +0530

    Add files via upload

---
+++ day21.py
@@ -0,0 +1,21 @@
+# Conditional Statements
+#if satement
+
+age = 12
+
+if(age >= 18):
+    print("CAN Apply")
+
+elif(age <= 18):
+    print("CANNOT APPLY")
+
+
+# elif statement
+
+
+
+
+
+
+
+    
