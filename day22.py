0afca89c42feb6c7f5dc91675c4f21c0f679c5ea
nibedita21-ai <nibeditaswain351@gmail.com>
2026-01-21 08:34:43 +0530

    Add files via upload

---
+++ day22.py
@@ -0,0 +1,13 @@
+# Conditional Statements
+#if satement and elif statement - 2nd question
+
+light = "red"
+
+if(light == "red"):
+    print("STOP")
+
+elif(light == "green"):
+    print("GO")
+
+elif(light == "yellow"):
+    print("SLOW DOWN")
