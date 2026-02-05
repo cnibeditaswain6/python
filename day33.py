73add43f4a50e0a919f0d7d786da7886005bea06
nibedita21-ai <nibeditaswain351@gmail.com>
2026-02-05 08:40:07 +0530

    Add files via upload

---
+++ day33.py
@@ -0,0 +1,9 @@
+
+# WAP to ask the user to enter names of their 3 favorite movies & store them in a list.
+
+movies = []
+movies.append(input("Enter the 1st fav movie : "))
+movies.append(input("Enter the 2nd fav movie : "))
+movies.append(input("Enter the 3rd fav movie : "))
+
+print(movies)
