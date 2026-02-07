4498a3cb26f3212d378fed9185699658780efa57
nibedita21-ai <nibeditaswain351@gmail.com>
2026-02-07 08:48:37 +0530

    Add files via upload

---
+++ day35.py
@@ -0,0 +1,10 @@
+# WAP to count the number of students with the "A" grade in the following tuple.
+
+grade = ("C","D","A","A","B","B","A")
+print("The Number of Students with 'A' Grade : ", grade.count("A"))
+
+# store the above values in a list & sort them from "A" to "D".
+
+list = ["C","D","A","A","B","B","A"]
+list.sort()
+print(list)
