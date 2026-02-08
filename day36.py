080263ba2c56514091fc53801f3e9c659ebe78e9
nibedita21-ai <nibeditaswain351@gmail.com>
2026-02-08 09:37:31 +0530

    Add files via upload

---
+++ day36.py
@@ -0,0 +1,28 @@
+# Dictionary and Set in Python
+
+# Dictionary in Python
+
+dict = {
+    "name" : "Nibedita",
+    "subjects" : ["Python", "C", "Java"],
+    "topics" : ("dict", "set"),
+    "age" : 35,
+    "is_adult" : True,
+    "marks": 94.4
+}
+dict["name"] = "C Nibedita Swain"
+print(dict["name"])
+print(dict["subjects"])
+print(dict["topics"])
+print(dict["age"])
+print(dict["is_adult"])
+print(dict["marks"])
+
+
+
+
+
+
+  
+
+ 
