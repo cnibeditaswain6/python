32feb77e2a2cbd2b03fdbcb0bfab42974f4f3a0c
nibedita21-ai <nibeditaswain351@gmail.com>
2026-02-09 08:36:31 +0530

    Add files via upload

---
+++ day37.py
@@ -0,0 +1,31 @@
+# adding another variable & changing the value of a variable in the dict - so dict are mutable
+dict = {
+    "name" : "Nibedita",
+    "subjects" : ["Python", "C", "Java"],
+    "topics" : ("dict", "set"),
+    "age" : 35,
+    "is_adult" : True,
+    "marks": 94.4
+}
+dict["name"] = "C Nibedita Swain"
+dict["gender"] = "Female"
+print(dict)
+
+
+# adding a null variable in the dict
+
+dict = {
+    "name" : "Nibedita",
+    "subjects" : ["Python", "C", "Java"],
+    "topics" : ("dict", "set"),
+    "age" : 35,
+    "is_adult" : True,
+    "marks": 94.4
+}
+
+print(dict)
+null_dict = {}
+print(null_dict)
+
+
+
