0fbd2bcb2fb6f7f4eb816c7042e86ae2ef708baf
nibedita21-ai <nibeditaswain351@gmail.com>
2026-02-11 09:06:52 +0530

    Add files via upload

---
+++ day38.py
@@ -0,0 +1,14 @@
+# Nested Dictionary
+
+student = {
+    "name" : "Nibedita Swain",
+    "age" : 22,
+    "subjects" : {
+        "dsa" : 54,
+        "ai" : 97,
+        "cd" : 76,
+        "se" : 55,
+    }
+}
+
+print(student)
