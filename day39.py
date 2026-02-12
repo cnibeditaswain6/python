a832e60229471539cc4fc120e286c6a3b7d0e4d2
nibedita21-ai <nibeditaswain351@gmail.com>
2026-02-12 20:25:59 +0530

    Add files via upload

---
+++ day39.py
@@ -0,0 +1,15 @@
+student = {
+    "name" : "Nibedita Swain",
+    "age" : 22,
+    "subjects" : {
+        "dsa" : 54,
+        "ai" : 97,
+        "cd" : 76,
+        "se" : 55,
+    }
+}
+
+print(list(student))
+print(student["subjects"])
+print(list(student["subjects"]))
+print(student["subjects"]["ai"])
