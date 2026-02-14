53737ecb5bb51d3cf69da0687a554a58b4f2403e
nibedita21-ai <nibeditaswain351@gmail.com>
2026-02-14 19:16:46 +0530

    Add files via upload

---
+++ day40.py
@@ -0,0 +1,18 @@
+# Dictionary Methods
+
+#1. keys() - returns all keys
+
+student = {
+    "name" : "Nibedita Swain",
+    "age" : 22,
+    "subjects" : {
+        "dsa" : 54,
+        "ai" : 97,
+        "cd" : 76,
+        "se" : 55,
+    }
+}
+
+print(student.keys())
+print(list(student.keys()))
+
