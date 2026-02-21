384f9bd35c78bca9c8fe30a9f49aad5a34854794
nibedita21-ai <nibeditaswain351@gmail.com>
2026-02-21 22:33:58 +0530

    Add files via upload

---
+++ day45.py
@@ -0,0 +1,18 @@
+# Set in python
+
+collection = {1, 2, 3, 4, "Hello", "World"}
+
+print(collection)
+print(type(collection))
+print(len(collection))
+
+#repeted elements stored only once, so it resolved to{1,2}
+
+
+# Empty set; syntax
+
+collection = set()
+
+print(type(collection))
+
+
