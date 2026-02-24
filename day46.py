d251b28c2cbe9cb7226ce3a066565b30f3ac26f9
nibedita21-ai <nibeditaswain351@gmail.com>
2026-02-24 20:45:48 +0530

    Add files via upload

---
+++ day46.py
@@ -0,0 +1,13 @@
+# Set Methods
+
+ #1.add(el) - add an element
+
+collection = set()
+collection.add(1)
+collection.add(2)
+collection.add("Nibedita")
+collection.add((1,2,3,4,5))
+
+print(collection)
+print(len(collection))
+
