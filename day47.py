e20c04862fd77aa570f9e6ac24ff8d11b43b6828
nibedita21-ai <nibeditaswain351@gmail.com>
2026-02-25 07:00:30 +0530

    Add files via upload

---
+++ day47.py
@@ -0,0 +1,13 @@
+#2. remove() - removes an element
+
+collection = set()
+collection.add(1)
+collection.add(2)
+collection.add("Nibedita")
+collection.add((1,2,3,4,5))
+
+collection.remove("Nibedita")
+print(collection)
+print(len(collection))
+
+   
