8131270dbf992761b970b054f836ff78cc9cae91
nibedita21-ai <nibeditaswain351@gmail.com>
2026-02-28 21:55:24 +0530

    Add files via upload

---
+++ day48.py
@@ -0,0 +1,13 @@
+#3. clear() - empties the set
+
+collection = set()
+collection.add(1)
+collection.add(2)
+collection.add("Nibedita")
+collection.add((1,2,3,4,5))
+
+collection.clear()
+print(collection)
+print(len(collection))
+
+   
