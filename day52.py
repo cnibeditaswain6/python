8faf00e4664d5561749997cca145011360a2929a
nibedita21-ai <nibeditaswain351@gmail.com>
2026-03-12 07:33:56 +0530

    Add files via upload

---
+++ day52.py
@@ -0,0 +1,10 @@
+#Q. Store following word meanings in a python dictionary:
+# table : "a piece of furniture", "list of facts & figures"
+# cat : "a small animal"
+
+dict = {
+    "cat" : "a small animal",
+    "table" : ["a peice of furniture", "list of facts & figures"]
+}
+
+print(dict)
