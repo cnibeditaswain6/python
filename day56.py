0426182843ee51c913a835714c0d3160aea2abf6
C Nibedita Swain <nibeditaswain351@gmail.com>
2026-03-19 19:46:42 +0530

    Add files via upload

---
+++ day56.py
@@ -0,0 +1,24 @@
+# Loops in python
+# loops are used to repeat instructions.
+
+count = 1
+while count <= 5 :
+    print("Nibedita")
+    count += 1
+
+print(count)
+
+
+
+i = 1
+while i <= 5:
+    print("Debasmita")
+    i += 1
+
+
+   
+  
+
+
+
+
